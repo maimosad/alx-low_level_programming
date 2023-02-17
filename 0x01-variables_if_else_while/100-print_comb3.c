fc4667431231f0067b63a7200d9af8cd5af7ebd0
maimosad <work_online_eg@yahoo.com>
2023-02-17 07:27:15 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,9 +2,7 @@
  * File: 100-print_comb3.c
  * Auth: Brennan D Baraban
  */
-
 #include <stdio.h>
-
 /**
  * main - Prints all possible combinations of two different digits,
  *        in ascending order, separated by a comma followed by a space.
