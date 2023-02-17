e0068134edd6037df80bbec1c7e2b18d755133fe
maimosad <work_online_eg@yahoo.com>
2023-02-17 07:29:54 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,6 @@
 int main(void)
 {
 	int digit1, digit2;
-
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
@@ -22,13 +21,10 @@ int main(void)
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
-			
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
