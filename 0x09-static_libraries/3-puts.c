02f12851608dcd78413cef8b94e59bf80b5d5627
maimosad <work_online_eg@yahoo.com>
2023-03-10 14:20:58 -0800

    m

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,17 +1,18 @@
 #include "main.h"
-#include<stdio.h>
 
 /**
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
  */
+
 void _puts(char *str)
 {
-	int i;
+int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; *str != '\0'; i++)
 	{
-		_putchar(str[i]);
+		_putchar(*str);
+		str++;
 	}
 	_putchar('\n');
 }
